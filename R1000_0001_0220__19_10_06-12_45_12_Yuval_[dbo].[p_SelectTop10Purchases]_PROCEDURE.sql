7b3f0a68e49e0ad648f4045d5582a2372b7028a7
Yuval2 <yperelman@hotmail.com>
2019-10-06 12:46:39 +0300

    twat

---
+++ R1000_0001_0220__19_10_06-12_45_12_Yuval_[dbo].[p_SelectTop10Purchases]_PROCEDURE.sql
@@ -0,0 +1,21 @@
+
+
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[p_SelectTop10Purchases]
+AS
+BEGIN
+	--waitfor delay '00:00:05'
+	--test
+	SELECT *
+    FROM (
+        SELECT *, Rank() 
+          over (Partition BY ProductID
+                ORDER BY LineTotal,PurchaseOrderDetailID DESC ) AS r
+        FROM [AdventureWorks2014].[Purchasing].[PurchaseOrderDetail]
+        ) rs WHERE r <=10
+END
